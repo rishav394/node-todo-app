79bda08d6645aa9bcc818721d60f0be9dad79d79
Rishav Rungta <rishav394@gmail.com>
2019-05-07 14:35:56 +0530

    refractoring

--- app.js
+++ app.js
@@ -9,55 +9,55 @@ app.set('view engine', 'ejs');
 app.use(express.static('.'));
 
 var urlencodedParser = bodyParser.urlencoded({
-    extended: false
+	extended: false,
 });
 
 var cred = 'username:password'; // NOTE: It should be urlencoded
 var port = process.env.PORT || 80;
 if (process.argv[2] !== undefined) {
-    cred = process.argv[2];
+	cred = process.argv[2];
 }
 
 mongoose.connect('mongodb://' + cred + '@ds133017.mlab.com:33017/mytodoapp');
 
 // Create a schema
 var schema = new mongoose.Schema({
-    item: String
+	item: String,
 });
 
 // Creating model
-var MyModel = mongoose.model("MyModel", schema);
+var MyModel = mongoose.model('MyModel', schema);
 
 app.get('/', (req, res) => {
-    res.send("hi");
+	res.redirect('/todo');
 });
 
 app.get('/todo', (req, res) => {
-    MyModel.find({}, function (err, data) {
-        if (err) throw err;
-        res.render('todoPage', {
-            'mydata': data
-        });
-    });
+	MyModel.find({}, function(err, data) {
+		if (err) throw err;
+		res.render('todoPage', {
+			mydata: data,
+		});
+	});
 });
 
 app.post('/todo', urlencodedParser, (req, res) => {
-    MyModel(req.body).save(function (err, data) {
-        if (err) throw err;
-        console.log('Item saved');
-        res.json(data);
-    });
+	MyModel(req.body).save(function(err, data) {
+		if (err) throw err;
+		console.log('Item saved');
+		res.json(data);
+	});
 });
 
 app.delete('/todo/:item', (req, res) => {
-    MyModel.find({
-        item: req.params.item
-    }).remove(function (err, data) {
-        if (err) throw err;
-        res.json(data);
-    });
+	MyModel.find({
+		item: req.params.item,
+	}).remove(function(err, data) {
+		if (err) throw err;
+		res.json(data);
+	});
 });
 
 app.listen(port, () => {
-    console.log('App listening on port ' + port + '!');
+	console.log('App listening on port ' + port + '!');
 });
--- assets/todo-list.js
+++ assets/todo-list.js
@@ -1,37 +1,34 @@
-$(document).ready(function () {
-	console.log("ok");
-	$('form').on('submit', function () {
-
+$(document).ready(function() {
+	console.log('ok');
+	$('form').on('submit', function() {
 		var item = $('form input');
 		// alert(item.val());
 		var todo = {
-			item: item.val()
+			item: item.val(),
 		};
-		console.log(todo.item + " in JS");
+		console.log(todo.item + ' in JS');
 		$.ajax({
 			type: 'POST',
 			url: '/todo',
 			data: todo,
-			success: function (data) {
+			success: function(data) {
 				//do something with the data via front-end framework
 				location.reload();
-			}
+			},
 		});
 
 		return false;
-
 	});
 
-	$('li').on('click', function () {
+	$('li').on('click', function() {
 		var item = $(this).text();
 		$.ajax({
 			type: 'DELETE',
 			url: '/todo/' + item,
-			success: function (data) {
+			success: function(data) {
 				//do something with the data via front-end framework
 				location.reload();
-			}
+			},
 		});
 	});
-
 });
